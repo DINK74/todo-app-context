f5e4a0c26e9399d4d63a79a5ca877eaa219651dd
Krzysztof Wlodarski <krzysztof.wlodarski@capgemini.com>
2023-01-03 21:07:10 +0100

    items filter implementation with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const initialTodos = [
@@ -19,12 +19,57 @@ const initialTodos = [
 	},
 ];
 
+const filterReducer = (state, action) => {
+	console.log(state, action.type);
+	switch (action.type) {
+		case "SHOW_ALL":
+			return "ALL";
+		case "SHOW_COMPLETE":
+			return "COMPLETE";
+		case "SHOW_INCOMPLETE":
+			return "INCOMPLETE";
+		default:
+			throw new Error();
+	}
+};
+
 function App() {
 	const [todos, setTodos] = useState(initialTodos);
 	const [task, setTask] = useState("");
+	const [filter, dispatchFilter] = useReducer(filterReducer, filterReducer(null, { type: "SHOW_ALL" }) /* ALL */);
+	
+	/* dispatchFilter(action) calls filterReducer(filter, action) and assigns return value to filter */
+
+	const filteredTodos = todos.filter((todo) => {
+		if (filter === "ALL") {
+			return true;
+		}
+
+		if (filter === "COMPLETE" && todo.complete) {
+			return true;
+		}
+
+		if (filter === "INCOMPLETE" && !todo.complete) {
+			return true;
+		}
+
+		return false;
+	});
 
 	const handleChangeInput = (event) => {
 		setTask(event.target.value);
+	};
+
+	const handleShowAll = () => {
+		dispatchFilter({ type: "SHOW_ALL" });
+	};
+
+	const handleShowComplete = () => {
+		dispatchFilter({ type: "SHOW_COMPLETE" });
+	};
+
+	const handleShowIncomplete = () => {
+		dispatchFilter({ type: "SHOW_INCOMPLETE" });
 	};
 
 	const handleChangeCheckbox = (id) => {
@@ -43,8 +88,19 @@ function App() {
 
 	return (
 		<div className="App">
+			<div>
+				<button type="button" onClick={handleShowAll}>
+					Show All
+				</button>
+				<button type="button" onClick={handleShowComplete}>
+					Show Complete
+				</button>
+				<button type="button" onClick={handleShowIncomplete}>
+					Show Incomplete
+				</button>
+			</div>
 			<ul>
-				{todos.map((todo) => (
+				{filteredTodos.map((todo) => (
 					<li key={todo.id}>
 						<input
 							type="checkbox"
